92f6c6cb8df5f5ff2dc46cce8c00b8eb82937e4b
Nashmod <hello@obsidianlab.io>
2023-11-02 22:16:49 -0400

    chore: start header style

--- src/components/navs/AppNav/AppNav.vue
+++ src/components/navs/AppNav/AppNav.vue
@@ -52,7 +52,11 @@ onUnmounted(() => {
 
 <template>
   <AppNavAlert v-if="currentAlert" :alert="currentAlert" />
-  <nav id="app-nav" ref="appNav" class="sticky top-0 lg:px-6 pr-1 pl-4 h-20">
+  <nav
+    id="app-nav"
+    ref="appNav"
+    class="xl:container sticky top-0 lg:px-6 pr-1 pl-4 mx-auto h-20"
+  >
     <div class="flex justify-between items-center h-full">
       <div class="flex items-center h-full">
         <router-link
@@ -62,9 +66,8 @@ onUnmounted(() => {
           <AppIcon v-if="['xs', 'sm'].includes(bp)" />
           <AppLogo v-else />
         </router-link>
-
-        <DesktopLinks v-if="isDesktop" class="ml-8 font-medium" />
       </div>
+      <DesktopLinks v-if="isDesktop" class="ml-8 font-medium" />
 
       <AppNavActions />
     </div>
